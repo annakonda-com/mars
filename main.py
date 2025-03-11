7c04520bf5f72b991231b681dc91496cc663cdc2
Anna <annakondr0709a@gmail.com>
2025-03-11 16:42:24 +0300

    Варианты выбора. Урок Введение во flask. Обработка HTML-форм

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index1(title):
-    return render_template("index.html", title=title)
-
-
-@app.route('/training/<prof>')
-def train(prof):
-    return render_template("index.html", title="Тренировки", prof=prof.lower())
+@app.route('/choice/<planet>')
+def choice(planet):
+    return render_template("index.html", title="Варианты выбора", planet=planet)
 
 
 if __name__ == '__main__':
