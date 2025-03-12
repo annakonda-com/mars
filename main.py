dfc7faf8d40cdac09af5f09cc1291b7e7f32d857
Anna <annakondr0709a@gmail.com>
2025-03-12 07:33:41 +0300

    Результат отбора. Урок Введение во flask

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
 
-@app.route('/choice/<planet>')
-def choice(planet):
-    return render_template("index.html", title="Варианты выбора", planet=planet)
+@app.route('/result/<nickname>/<int:level>/<float:rating>')
+def choice(nickname, level, rating):
+    return render_template("index.html", nickname=nickname, level=level, rating=rating)
 
 
 if __name__ == '__main__':
