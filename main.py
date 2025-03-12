427f3b52c0fdbe20a974a28b5ed2003fb5889b93
Anna <annakondr0709a@gmail.com>
2025-03-12 17:36:03 +0300

    Галерея с загрузкой. Шаблоны

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, redirect, render_template, request
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/galery/', methods=['POST', 'GET'])
 def index1():
+    images = os.listdir('./static/img')
     if request.method == 'POST':
         f = request.files['file']
-        print(f.read())
-    return render_template("index.html")
+        save = f.read()
+        new_name = images[-1].split('.')[0][:-1] + str(int(images[-1].split('.')[0][-1]) + 1)
+        with open(f'static/img/{new_name}', mode="wb") as file:
+            file.write(save)
+        images = os.listdir('./static/img')
+    return render_template("index.html", images=images[1:])
 
 
 if __name__ == '__main__':
