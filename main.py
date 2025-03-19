93a98ce23374490cea87a8477a7a56cb4e0b0bbf
Anna <annakondr0709a@gmail.com>
2025-03-19 18:19:22 +0300

    Журнал работ

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, redirect, render_template, request, url_for
 import os
 import json
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
 
 from random import choice
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+db_session.global_init("db/mars_explorer.db")
 
 
 @app.route('/galery/', methods=['POST', 'GET'])
@@ -49,10 +54,22 @@ def image_mars():
                 f.write(request.files['file'].read())
             return render_template('load_photo.html', image='astro_photo')
 
+
 @app.route('/carousel')
 def carousel():
     images = os.listdir('./static/img')
     return render_template("index.html", images=images[1:])
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    names = dict()
+    for job in jobs:
+        user = db_sess.query(User).filter(User.id == job.team_leader).first()
+        names[job.team_leader] = user.surname + ' ' + user.name
+    return render_template("new_index.html", jobs=jobs, names=names, title="Марсиане")
 
 
 if __name__ == '__main__':
