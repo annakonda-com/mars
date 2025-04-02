bc01ded6cb8055cef0223343ada21714d91fc5e0
Anna <annakondr0709a@gmail.com>
2025-04-02 18:27:28 +0300

    for deploy

--- server.py
+++ server.py
@@ -222,4 +222,5 @@ def job_delete(id):
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
