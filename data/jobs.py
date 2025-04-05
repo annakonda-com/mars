ec071c6c07b681f06411075b70cc14967027ab2f
Anna <annakondr0709a@gmail.com>
2025-04-05 19:33:25 +0300

    deploy

--- data/jobs.py
+++ data/jobs.py
@@ -2,6 +2,7 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
 
 
 class Jobs(SqlAlchemyBase, SerializerMixin):
